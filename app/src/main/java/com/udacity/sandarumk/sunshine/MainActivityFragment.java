2cea9d8f5fafe4ab133304511c7f23ee5a0155a8
Dinu Kumarasiri <sandarumk@gmail.com>
2016-06-22 21:20:16 +0530

    Added list view and elements

--- app/src/main/java/com/udacity/sandarumk/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/udacity/sandarumk/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Adding dummy data for the list view
+        List<String> data = new ArrayList();
+        data.add("Mon 6/23 - Sunny - 31/17");
+        data.add("Tue 6/24 - Foggy - 21/8");
+        data.add("Wed 6/25 - Cloudy - 22/17");
+        data.add("Thurs 6/26 - Rainy - 18/11");
+        data.add("Fri 6/27 - Foggy - 21/10");
+        data.add("Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18");
+        data.add("Sun 6/29 - Sunny - 20/7");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), R.layout.list_item_forcast,R.id.list_item_forecast_textview,data);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
+        listView.setAdapter(arrayAdapter);
+        return rootView;
     }
 }
