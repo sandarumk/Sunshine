728c5ac1915abb109fccac87035a538fe56ffa62
Dinu Kumarasiri <sandarumk@gmail.com>
2016-07-17 21:09:30 +0530

    Fixed the detail view issue

--- app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivityFragment.java
@@ -1,5 +1,6 @@
 package com.udacity.sandarumk.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,14 +25,10 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        if (container== null){
-            Toast.makeText(getContext(),"Container is null",Toast.LENGTH_SHORT).show();
-        }
-        Toast.makeText(getContext(),"Container is not null",Toast.LENGTH_SHORT).show();
+
         View rootview = inflater.inflate(R.layout.fragment_detail, container, false);
         TextView textView = (TextView) rootview.findViewById(R.id.text_view_detail_Weather);
-        textView.setText("Modaya");
-        //textView.setText(getActivity().getIntent().getExtras().getString(Intent.EXTRA_TEXT));
+        textView.setText(getActivity().getIntent().getExtras().getString(Intent.EXTRA_TEXT));
         return rootview;
     }
 }
