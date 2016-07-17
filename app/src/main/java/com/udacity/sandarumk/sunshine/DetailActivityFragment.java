c15000d98e046f8da73c08b3e849ba1de3627d96
Dinu Kumarasiri <sandarumk@gmail.com>
2016-07-17 20:16:10 +0530

    Added the fragment to content xml

--- app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivityFragment.java
@@ -1,11 +1,13 @@
 package com.udacity.sandarumk.sunshine;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,8 +18,17 @@ public class DetailActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (container== null){
+            Toast.makeText(getContext(),"Container is null",Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(getContext(),"Container is not null",Toast.LENGTH_SHORT).show();
         View rootview = inflater.inflate(R.layout.fragment_detail, container, false);
         TextView textView = (TextView) rootview.findViewById(R.id.text_view_detail_Weather);
         textView.setText("Modaya");
