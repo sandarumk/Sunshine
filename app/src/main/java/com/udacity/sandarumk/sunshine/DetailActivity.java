1452ed49ec6b3a1429c3745364ef17d1328f6d26
Dinu Kumarasiri <sandarumk@gmail.com>
2016-07-31 20:59:04 +0530

    Added ability to sharing

--- app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivity.java
+++ app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivity.java
@@ -3,11 +3,15 @@ package com.udacity.sandarumk.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ShareActionProvider mShareActionProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,16 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.detail, menu);
+
+        // Return true to display menu
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -40,6 +48,5 @@ public class DetailActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 }
--- app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/udacity/sandarumk/sunshine/DetailActivityFragment.java
@@ -4,15 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import static com.udacity.sandarumk.sunshine.R.menu.detail_fragment;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
+
+    private ShareActionProvider mShareActionProvider;
+    private String forecast;
 
     public DetailActivityFragment() {
     }
@@ -30,9 +40,41 @@ public class DetailActivityFragment extends Fragment {
         TextView textView = (TextView) rootview.findViewById(R.id.text_view_detail_Weather);
         Intent detailViewInent = getActivity().getIntent();
         if (detailViewInent!=null && detailViewInent.hasExtra(Intent.EXTRA_TEXT)){
-            textView.setText(getActivity().getIntent().getExtras().getString(Intent.EXTRA_TEXT));
+            forecast =getActivity().getIntent().getExtras().getString(Intent.EXTRA_TEXT);
+            textView.setText(forecast);
         }
 
         return rootview;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate menu resource file.
+        inflater.inflate(detail_fragment, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        mShareActionProvider = new ShareActionProvider(getActivity());
+        // Fetch and store ShareActionProvider
+        mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        setShareIntent(createShareIntent());
+        // Return true to display menu
+
+    }
+    private Intent createShareIntent(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,forecast+"#Sunshine");
+        return intent;
+    }
+
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 }
